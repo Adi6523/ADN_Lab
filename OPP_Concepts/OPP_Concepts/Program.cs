fcc645558115cd1b3be2ec605f3c9cda346faf93
Aditya Patil <adityap192005@gmail.com>
2026-01-28 09:59:26 +0530

    1st and 2nd experiment

---
+++ OPP_Concepts/OPP_Concepts/Program.cs
@@ -0,0 +1,105 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//class
+
+//class Car
+//{
+//    string color = "red";
+
+//    static void Main(string[] args)
+//    {
+//        Car c1 = new Car();
+//        Console.WriteLine(c1.color);
+//    }
+//}
+
+
+//class Members
+//class Car
+//{
+//    string color = "red";
+//    int maxSpeed = 200;
+
+//    static void Main(string[] args)
+//    {
+//        Car myObj = new Car();
+//        Console.WriteLine(myObj.color);
+//        Console.WriteLine(myObj.maxSpeed);
+//    }
+//}
+
+//constructor
+// Create a Car class
+//class Car
+//{
+//    public string model;  // Create a field
+
+//    // Create a class constructor for the Car class
+//    public Car()
+//    {
+//        model = "Mustang"; // Set the initial value for model
+//        model = "Audi";
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+//        Console.WriteLine(Ford.model);  // Print the value of model
+//    }
+//}
+
+// Outputs "Mustang"
+
+//GET SET METHODS
+
+//class Person
+//{
+//    private string name; // field
+//    public string Name   // property
+//    {
+//        get { return name; }
+//        set { name = value; }
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Person myObj = new Person();
+//        myObj.Name =Console.ReadLine();
+//        Console.WriteLine(myObj.Name);
+//    }
+//}
+
+
+//inheritance
+class Vehicle  // base class (parent) 
+{
+    public string brand = "Ford";  // Vehicle field
+    public void honk()             // Vehicle method 
+    {
+        Console.WriteLine("Tuut, tuut!");
+    }
+}
+
+class Car : Vehicle  // derived class (child)
+{
+    public string modelName = "Mustang";  // Car field
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a myCar object
+        Car myCar = new Car();
+
+        // Call the honk() method (From the Vehicle class) on the myCar object
+        myCar.honk();
+
+        // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+        Console.WriteLine(myCar.brand + " " + myCar.modelName);
+    }
+}
---
+++ Solid_principles/Solid_principles/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
+class student
+{
+    public void savestudent() { 
+        //student save in db
+     }
+    public void printreport() { 
+        //print student
+    }
+}
+//bad example of coding /\
+
+
+//Good way of SRP
+
+class student2
+{
+    public String name {  get; set; }
+}
+
+class savestudentdata
+{
+    public void savestudentdb(student std) { }
+}
+
+class reportstudent
+{
+    public void printreport() { }
+}
+
+//OCP
+
+class discount
+{
+    public DoublegetDiscount(String customerType)
+    {
+        if ()
+            return;
+        else
+    }
+}
+
+
+//best ex of OCP
+//to add new discount add new class no need to change before code 
+interface IdDiscount
+{
+    getDiscount(Return;)
+}
+
+class studentDiscount : IdDiscount
+{
+    public getDiscount()=>10
+}
+
+class teacherDiscount : IdDiscount
+{
+    public getDiscount()=>20
+}
+
+
+//LSP
+
+class bird
+{
+    public virtual void fly()
+    {
+
+    }
+}
+
+class ostrich : bird
+{
+    public override void fly()
+    {
+
+    }
+}
+
+
